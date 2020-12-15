75c51b20d24f103974e675e55f981c39654253e9
shreyansh-jain <shreyanshbafna.310@gmail.com>
2020-12-15 07:26:42 +0530

    image in feed

--- lib/InstaList.dart
+++ lib/InstaList.dart
@@ -10,7 +10,47 @@ class InstaList extends StatelessWidget {
         itemBuilder: (context, index) => index == 0
             ? SizedBox(child: InstaStories(), height: deviceSize.height * 0.15)
             : Column(
-              
-            ));
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Container(
+                              height: 40.0,
+                              width: 40.0,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          'https://images.unsplash.com/photo-1573729030734-10fca8f2003b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'),
+                                      fit: BoxFit.fill)),
+                            ),
+                            SizedBox(width: 10.0),
+                            Text("_shreyanhbafna_",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ],
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.more_vert),
+                          onPressed: null,
+                        )
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                      fit: FlexFit.loose,
+                      child: Image.network(
+                          'https://images.unsplash.com/photo-1573729030734-10fca8f2003b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+                          fit: BoxFit.fill))
+                ],
+              ));
   }
 }
